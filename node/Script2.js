609666fc93e5e035b6de4e4eb2b733a9c5a98056
M-SK <legendary512@gmail.com>
2022-12-07 02:12:00 +0300

    .

---
+++ node/Script2.js
@@ -0,0 +1,12 @@
+const Script2 = () => {
+
+    return 'Script2 is working!';
+}
+
+// An old way of exporting things.
+/*
+module.exports = {
+    Script2: Script2
+};*/
+
+export {Script2};
--- node/script.js
+++ node/script.js
@@ -1,9 +1,23 @@
 const a = 4;
 const b = 5;
 
+// Just try running below code using node.js!
 function fun(){
     console.log(a+b)
 }
-setTimeout(fun, 5000);
 
-setTimeout(()=>{console.log(a+b)}, 5000);
+setTimeout(fun, 2000);
+setTimeout(()=>{console.log(a+b)}, 2000);
+
+// You can display directory using the command below
+//console.log(__dirname);
+
+// An old way of importing things from other files
+/*
+const large = require('./Script2.js')
+console.log(large.Script2());*/
+
+// A new way of importing things (Using ES6 modules)
+import {Script2} from './Script2.js';
+
+console.log(Script2());
