7d21b1bd861be0223b6bc2a47087d2ac213ab5dc
M-SK <legendary512@gmail.com>
2022-12-07 17:49:00 +0300

    .

--- node/Type_of_modules/main.js
+++ node/Type_of_modules/main.js
@@ -1,3 +1,5 @@
-const c =require('fs');
+const module_fs = require('fs'); // A module with bunch of stuff that could be used to read strings in text files.
+const module_http = require('http'); // Used to build an http server.
+//const module_nodmon = require('nodemon')
 
-console.log(c);
+console.log(module_fs);
