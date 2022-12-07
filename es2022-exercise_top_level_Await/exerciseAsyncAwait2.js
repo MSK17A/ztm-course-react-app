ad84bac2fcc9f024ae764f7ec8ba88d901f30bf9
MSK17A <legendary512@gmail.com>
2022-12-07 09:56:24 +0300

    .

--- es2022-exercise_top_level_Await/exerciseAsyncAwait2.js
+++ es2022-exercise_top_level_Await/exerciseAsyncAwait2.js
@@ -31,14 +31,13 @@ const urls = [
 
 // --------- Answer:
 
-const getData = async function () {
-  const [users, posts, albums] = await Promise.all(
-    urls.map((url) => fetch(url).then((resp) => resp.json())),
-  );
-  console.log("users", users);
-  console.log("posta", posts);
-  console.log("albums", albums);
-  }
+[users, posts, albums] = await Promise.all(
+  urls.map((url) => fetch(url).then((resp) => resp.json())),
+);
+console.log("users", users);
+console.log("posta", posts);
+console.log("albums", albums);
+
 
 // #3  ADVANCED: Try to run this piece of code just as a JS file not inside the browser dev tool, instea using this:
 //https://glot.io/new/javascript
