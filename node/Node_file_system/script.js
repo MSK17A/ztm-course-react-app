191a9817536cc836f877ea04f14ed24512af7333
MSK17A <legendary512@gmail.com>
2022-12-08 23:17:53 +0300

    .

--- node/Node_file_system/script.js
+++ node/Node_file_system/script.js
@@ -5,7 +5,10 @@ Function readFile will give params to the callback function which in this case '
 you just do what you want to do with these params inside the function */
 
 function printBuffer(err, buf) {
-    console.log("String 1: " + buf.toString("utf8")); // utf8 is a type of encoding that is a standard on the web.
+    if (err)
+        console.log(err);
+    else
+        console.log("String 1: " + buf.toString("utf8")); // utf8 is a type of encoding that is a standard on the web.
 }
 fs.readFile('file.txt', printBuffer);
 
@@ -22,3 +25,23 @@ In 'readFileSync' however, it will say "stop what you are doing and wait for me 
 continue. It as a syncronous function."
 
 That is why you see "String 2" printed before "String 1". */
+
+// APPEND
+/*
+fs.appendFile('./file.txt', ' This is so cooool!, sunvabch', err=>{
+    console.log(err);
+}) */
+
+// WRITE
+
+fs.writeFile("./bye.txt", "Sad to see you go :(", (err) => {
+    if (err)
+        console.log(err);
+})
+
+// DELETE
+
+fs.unlink('./bye.txt', (err) => {
+    if (err)
+        console.log(err);
+})
