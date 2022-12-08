174714fdd7d4760de591449de9e1cd9c9effe076
M-SK <legendary512@gmail.com>
2022-12-09 02:49:53 +0300

    .

--- Advent_of_Code_excercises/2015/Day_1/script.js
+++ Advent_of_Code_excercises/2015/Day_1/script.js
@@ -1,21 +1,44 @@
-const {getInput} = require('./helperFuncs.js');
+const { getInput } = require('./helperFuncs.js');
 
 /* Get floor */
-getInput('https://adventofcode.com/2015/day/1/input', getFloor)
+getInput('https://adventofcode.com/2015/day/1/input', [getFloor, getBasementChar]); // Will supply input data to the callbacks.
 
-function getFloor(inputData='') {
-    console.log(inputData);
+// Question 1
+function getFloor(inputData = '') {
+    //console.log(inputData);
 
     console.time('Execution time')
     let floor = 0;
     for (let index = 0; index < inputData.length; index++) {
-        if(inputData[index] === '(')
-        floor++;
-        else if(inputData[index] === ')')
-        floor--;
-        
+        if (inputData[index] === '(')
+            floor++;
+        else if (inputData[index] === ')')
+            floor--;
+
     }
 
     console.log("Floor: " + floor);
+    console.timeEnd('Execution time');
+}
+
+// Question 2
+function getBasementChar(inputData = '') {
+
+    console.time('Execution time')
+    let floor = 0;
+    let charPos = 0;
+    for (let index = 0; index < inputData.length; index++) {
+        if (inputData[index] === '(')
+            floor++;
+        else if (inputData[index] === ')')
+            floor--;
+        if (floor === -1) {
+            charPos = index+1;
+            break;
+        }
+
+    }
+
+    console.log("Character position: " + charPos);
     console.timeEnd('Execution time');
 }
